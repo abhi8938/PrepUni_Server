c60937a834f561e3274a3c3f86c45585e5de0f21
roopesh04 <arvapalli12345@gmail.com>
2021-03-13 19:15:07 +0530

    Updated the database link

--- src/Configs/mongo_connection.mjs
+++ src/Configs/mongo_connection.mjs
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connect_db = () => {
   mongoose
-    .connect("mongodb://localhost/prepuni", {
+    .connect("mongodb+srv://admin_prep:waFHuMrPzPpwbjeq@prepuni.aj9c2.mongodb.net/test", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
