529cc90ddfb0f43dc6de734fb69760732d78710f
Abhishek Singh <abhishek8938@gamil.com>
2021-03-22 16:53:02 +0530

    transferred ownership

--- src/Middlewares/error.mjs
+++ src/Middlewares/error.mjs
@@ -1,8 +1,8 @@
-import winston from 'winston';
+import winston from "winston";
 
-const error=function (err,req,res,next) {
-    winston.log(err.message,err)
-    res.status(500).send(err.message);
-  }
+const error = function (err, req, res, next) {
+  winston.log(err.message, err);
+  res.status(201).send(err.message);
+};
 
 export default error;
