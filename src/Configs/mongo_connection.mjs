0face37986aa89dfda3f7a94d241b475c37b6fb0
Abhishek Singh <abhishek8938@gamil.com>
2021-03-23 17:35:51 +0530

    changes in student

--- src/Configs/mongo_connection.mjs
+++ src/Configs/mongo_connection.mjs
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
-
+//localhost:27017/prepuni
 // main_link="mongodb+srv://admin_prep:waFHuMrPzPpwbjeq@prepuni.aj9c2.mongodb.net/prepuni?retryWrites=true&w=majority"
 export const connect_db = () => {
   mongoose
-    .connect("mongodb://localhost:27017/prepuni", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
+    .connect(
+      "mongodb+srv://admin_prep:waFHuMrPzPpwbjeq@prepuni.aj9c2.mongodb.net/prepuni?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    )
     .then(() => console.log("connected to Database...."))
     .catch((err) =>
       console.log("Connection refused to database because....", err)
