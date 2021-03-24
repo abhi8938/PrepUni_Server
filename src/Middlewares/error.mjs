f4205e8da4f86bce0f0c678fe43bec4c2a783380
Abhishek Singh <abhishek8938@gamil.com>
2021-03-24 14:05:06 +0530

    final code test

--- src/Middlewares/error.mjs
+++ src/Middlewares/error.mjs
@@ -3,7 +3,9 @@ import winston from "winston";
 const error = function (error, req, res, next) {
   winston.log(error.message, error);
   console.log("error", error);
-  res.status(400).send({ error });
+  res.status(500).json({
+    message: error.message || "INTERNAL SERVER ERROR",
+  });
 };
 
 export default error;
