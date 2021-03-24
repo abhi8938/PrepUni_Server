1466f117810a32075bfe351abaa519f6f7c88471
Abhishek Singh <abhishek8938@gamil.com>
2021-03-24 13:54:46 +0530

    final attempt

--- src/Middlewares/error.mjs
+++ src/Middlewares/error.mjs
@@ -1,8 +1,9 @@
 import winston from "winston";
 
-const error = function (err, req, res, next) {
-  winston.log(err.message, err);
-  res.status(400).send(err.message);
+const error = function (error, req, res, next) {
+  winston.log(err.message, error);
+  console.log("error", error);
+  res.status(400).send({ error });
 };
 
 export default error;
