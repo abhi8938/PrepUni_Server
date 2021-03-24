80896dcfe022b142215138bd1ac123c65c425c25
Abhishek Singh <abhishek8938@gamil.com>
2021-03-24 13:56:52 +0530

    error status code

--- src/Middlewares/error.mjs
+++ src/Middlewares/error.mjs
@@ -1,7 +1,7 @@
 import winston from "winston";
 
 const error = function (error, req, res, next) {
-  winston.log(err.message, error);
+  winston.log(error.message, error);
   console.log("error", error);
   res.status(400).send({ error });
 };
