fb13ba1bd4b461e786ed41fcc8215a861331f555
Abhishek Singh <abhishek8938@gamil.com>
2021-03-26 20:17:33 +0530

    token to 201

--- src/Middlewares/auth.mjs
+++ src/Middlewares/auth.mjs
@@ -2,13 +2,13 @@ import config from "config";
 import jwt from "jsonwebtoken";
 const auth = (req, res, next) => {
   const token = req.header("x-auth-token");
-  if (!token) return res.status(400).send("Access denied. No token given.");
+  if (!token) return res.status(201).send("Access denied. No token given.");
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(400).send("Invalid token. Login Again!");
+    return res.status(201).send("Invalid token. Login Again!");
   }
 };
 
