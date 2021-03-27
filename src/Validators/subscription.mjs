b7355d59df80d69a0e715ae85c0618324809ae1b
Abhishek Singh <abhishek8938@gamil.com>
2021-03-27 13:10:31 +0530

    subscription changes

--- src/Validators/subscription.mjs
+++ src/Validators/subscription.mjs
@@ -4,46 +4,49 @@ import mongoose from "mongoose";
 
 export const Subscript = mongoose.model(
   "subscription",
-  new mongoose.Schema({
-    STID: {
-      type: mongoose.Schema.ObjectId,
-      required: true,
-      unique: true,
-    },
-    PID: {
-      type: mongoose.Schema.ObjectId,
-      required: true
-    },
-    program_id: {
+  new mongoose.Schema(
+    {
+      STID: {
         type: mongoose.Schema.ObjectId,
-        required: true
+        required: true,
+        unique: true,
       },
-    type: {
-      type: String,
-      required: true,
+      PID: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+      },
+      program_id: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+      },
+      type: {
+        type: String,
+        required: true,
+      },
+      PA_ID: {
+        type: mongoose.Schema.ObjectId,
+      },
+      expiration: {
+        type: Date,
+        required: true,
+      },
+      status: {
+        type: String,
+        enum: ["ACTIVE", "INACTIVE"],
+        default: "INACTIVE",
+      },
+      DUR: [DUR],
     },
-    PA_ID: {
-      type: mongoose.Schema.ObjectId,
-    },
-    expiration: {
-      type: Date,
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ["ACTIVE", "INACTIVE"],
-      default: "INACTIVE",
-    },
-    DUR: [DUR],
-  },{
-    timestamps:true
-  })
+    {
+      timestamps: true,
+    }
+  )
 );
 
 export const validate = (subscription) => {
   const schema = Joi.object({
     PID: Joi.string().required(),
-    program_id:Joi.string(),
+    program_id: Joi.string(),
     type: Joi.string(),
     PA_ID: Joi.string(),
     status: Joi.string(),
@@ -55,7 +58,7 @@ export const validate = (subscription) => {
 export const validateUpdate = (subscription) => {
   const schema = Joi.object({
     status: Joi.string().valid("ACTIVE", "INACTIVE"),
-    subjects: Joi.array().items(Joi.string())
+    subjects: Joi.array().items(Joi.string()),
   });
 
   return schema.validate(subscription);
