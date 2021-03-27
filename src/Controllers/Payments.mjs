76fe2b9866d0d1062b18f5768d4616d03d93c18d
Abhishek Singh <abhishek8938@gamil.com>
2021-03-27 19:42:47 +0530

    main flow

--- src/Controllers/Payments.mjs
+++ src/Controllers/Payments.mjs
@@ -1,7 +1,7 @@
 import { Payment, validate, validateUpdate } from "../Validators/payments.mjs";
 
 import Razorpay from "razorpay";
-import crypto from "crypto"
+import crypto from "crypto";
 
 var instance = new Razorpay({
   key_id: "rzp_test_JrmprfPdb6LHFI",
@@ -48,23 +48,26 @@ export const update_payment = async (req, res) => {
   });
 
   if (!payment)
-  throw new Error("The Payment with the given id is not available");
+    throw new Error("The Payment with the given id is not available");
 
   const order_id = payment.order_id;
-  const razorpay_signature = payment.razorpay_signature
-  const razorpay_payment_id=payment.razorpay_payment_id
+  const razorpay_signature = payment.razorpay_signature;
+  const razorpay_payment_id = payment.razorpay_payment_id;
 
-  const hmac = crypto.createHmac('sha256', "Ut6lmneQIQXOufkeflo4jHFC");
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
 
-hmac.update(order_id + "|" + razorpay_payment_id);
-let generatedSignature = hmac.digest('hex');
+  let expectedSignature = crypto
+    .createHmac("sha256", "Ut6lmneQIQXOufkeflo4jHFC")
+    .update(body.toString())
+    .digest("hex");
+  console.log("sig" + razorpay_signature);
+  console.log("sig_e" + expectedSignature);
 
-let isSignatureValid = generatedSignature === razorpay_signature;
+  if (!expectedSignature === razorpay_signature)
+    throw new Error("Invalida siganture");
 
- if(!isSignatureValid) throw new Error('Invalid Razorpay Payment Signature')
-
- payment.status="SUCCESS"
- await payment.save()
+  payment.status = "SUCCESS";
+  await payment.save();
 
   res.status(200).send(payment);
 };
