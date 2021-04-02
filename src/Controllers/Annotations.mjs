3423cb722377468c14984046c540c8d3e3e26db9
roopesh04 <arvapalli12345@gmail.com>
2021-04-02 23:55:30 +0530

    Updated the annotations router

--- src/Controllers/Annotations.mjs
+++ src/Controllers/Annotations.mjs
@@ -5,20 +5,25 @@ import {
 } from "../Validators/annotations.mjs";
 
 export const get_annotations = async (req, res) => {
-  const annotations = await Annotations.find().sort("pageCfi");
+  const annotations = await Annotations.find({STID:req.user._id}).sort("pageCfi");
   res.send(annotations);
 };
 
 export const get_annotation = async (req, res) => {
-  const annotation = await Annotations.findById(req.user._id);
+  const annotation = await Annotations.find({STID:req.user._id,paper_id:req.params.id}).sort("pageCfi");
   if (!annotation) throw new Error("Invald Id");
+  res.status(200).send(annotation)
 };
 
 export const post_annotations = async (req, res) => {
+  req.body.STID=req.user._id
   const { error } = Validate(req.body);
   if (error) throw new Error(error.details[0].message);
 
-  let annotations = new annotations(req.body);
+  const duplicate=await Annotations.findOne({paper_id:req.body.paper_id,STID:req.user._id})
+  if(duplicate) throw new Error("Data is aldredy uplaoded under this paper try  editing it")
+
+  let annotations = new Annotations(req.body);
 
   annotations = await annotations.save();
 
--- src/Validators/common.mjs
+++ src/Validators/common.mjs
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 export const DUR = {
   key: {
     type: String,
